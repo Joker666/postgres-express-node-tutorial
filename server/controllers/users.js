c5880bbc4dca066c6ccee581c169b9baa9f4f6ec
Hasan Rafi <mr.k779@outlook.com>
2020-12-23 04:29:17 +0600

    Creating user by hashing password

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,17 +1,23 @@
 const Organization = require('../models').Organization;
 const User = require('../models').User;
+const bcrypt = require('bcrypt');
 
 module.exports = {
   create(req, res) {
     return Organization
-      .create({
-        title: req.body.title,
-        // name: req.body.name,
-        // email: req.body.email,
-        // password: req.body.password,
-        // organizationId: req.body.organizationId,
+      .findOrCreate({ where: { title: req.body.organization_name } })
+      .spread((org, created) => {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            organizationId: org.id,
+          })
+          .then((user) => res.status(201).send(user))
+          .catch((error) => res.status(400).send(error));
+        });
       })
-      .then((user) => res.status(201).send(user))
       .catch((error) => res.status(400).send(error));
   },
 };
