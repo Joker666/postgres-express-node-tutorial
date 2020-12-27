e3604b9f9d20279a168abd2fe422c262515426cf
Hasan Rafi <mr.k779@outlook.com>
2020-12-27 22:30:57 +0600

    Part 1

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const db = require('./server/models/index');
 
 const app = express();
 const router = express.Router();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 require('./server/routes')(router);
 app.use('/', router);
